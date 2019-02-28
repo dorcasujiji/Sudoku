7bd0c69541de20da1ddeaa6ab4f59854a4250dd6
Dorcas Ujiji <dujiji@smith.edu>
2019-02-27 19:17:36 -0500

    complete functionality with slight changes to command

--- src/Board.java
+++ src/Board.java
@@ -3,8 +3,8 @@ import java.util.Random;
 
 public class Board {
     /* Game board*/
-    public int[][] board;
-    public int[][] solution;
+    private int[][] board;
+    private int[][] solution;
 
     /* Initial game entry stored by array index e.g. [0][3]*/
     private HashSet<String> set = new HashSet<>();
@@ -13,32 +13,29 @@ public class Board {
     * set need to be initialized if first time printing board */
     private boolean firstPrint = true;
 
-    /*tracks which board was picked so we can know what solution to pick*/
-    int boardPicked;
-
     /* possible initial game boards */
     private int[][] gameBoardOne = {
-            {0, 5, 8, 0, 0, 0, 0, 4, 0}, 
-            {0, 0, 0, 0, 0, 7, 0, 0, 6}, 
-            {7, 0, 0, 9, 0, 5, 0, 2, 1}, 
-            {0, 4, 2, 6, 0, 9, 0, 0, 0}, 
-            {0, 7, 0, 1, 4, 8, 0, 3, 0}, 
-            {0, 0, 0, 5, 0, 2, 9, 6, 0}, 
-            {6, 9, 0, 2, 0, 3, 0, 0, 8}, 
-            {8, 0, 0, 7, 0, 0, 0, 0, 0},
-            {0, 1, 0, 0, 0, 0, 2, 9, 0}
+            {0, 0, 0, 2, 6, 0, 7, 0, 1},
+            {6, 8, 0, 0, 7, 0, 0, 9, 0},
+            {1, 9, 0, 0, 0, 4, 5, 0, 0},
+            {8, 2, 0, 1, 0, 0, 0, 4, 0},
+            {0, 0, 4, 6, 0, 2, 9, 0, 0},
+            {0, 5, 0, 0, 0, 3, 0, 2, 8},
+            {0, 0, 9, 3, 0, 0, 0, 7, 4},
+            {0, 4, 0, 0, 5, 0, 0, 3, 6},
+            {7, 0, 3, 0, 1, 8, 0, 0, 0}
             };
     
     private int[][] gameBoardTwo = {
-            {6, 0, 0, 0, 4, 0, 1, 0, 0},
-            {0, 4, 3, 0, 6, 0, 0, 0, 0},
-            {0, 7, 1, 0, 0, 0, 8, 0, 4},
-            {0, 8, 0, 0, 0, 9, 0, 0, 0},
-            {0, 3, 0, 5, 0, 6, 0, 2, 0},
-            {0, 0, 0, 8, 0, 0, 0, 4, 0},
-            {1, 0, 7, 0, 0, 0, 2, 8, 0},
-            {0, 0, 0, 0, 7, 0, 5, 9, 0},
-            {0, 0, 2, 0, 8, 0, 0, 0, 7}
+            {1, 0, 0, 4, 8, 9, 0, 0, 6},
+            {7, 3, 0, 0, 0, 0, 0, 4, 0},
+            {0, 0, 0, 0, 0, 1, 2, 9, 5},
+            {0, 0, 7, 1, 2, 0, 6, 0, 0},
+            {5, 0, 0, 7, 0, 3, 0, 0, 8},
+            {0, 0, 6, 0, 9, 5, 7, 0, 0},
+            {9, 1, 4, 6, 0, 0, 0, 0, 0},
+            {0, 2, 0, 0, 0, 0, 0, 3, 7},
+            {8, 0, 0, 5, 1, 2, 0, 0, 4}
     };
     private int[][] gameBoardThree = {
             {2, 0, 5, 0, 0, 7, 0, 0, 6},
@@ -50,6 +47,28 @@ public class Board {
             {7, 5, 0, 0, 2, 0, 0, 0, 0},
             {0, 6, 0, 0, 5, 1, 0, 0, 2},
             {3, 0, 0, 4, 0, 0, 5, 0, 8}
+    };
+    private int[][] gameBoardOneSolution = {
+            {4, 3, 5, 2, 6, 9, 7, 8, 1},
+            {6, 8, 2, 5, 7, 1, 4, 9, 3},
+            {1, 9, 7, 8, 2, 4, 5, 6, 2},
+            {8, 2, 6, 1, 9, 5, 3, 4, 7},
+            {3, 7, 4, 6, 8, 2, 9, 1, 5},
+            {9, 5, 1, 7, 4, 3, 6, 2, 8},
+            {5, 1, 9, 3, 2, 6, 8, 7, 4},
+            {2, 4, 8, 9, 5, 7, 1, 3, 6},
+            {7, 6, 3, 4, 1, 8, 2, 5, 9}
+    };
+    private int[][] gameBoardTwoSolution = {
+            {1, 5, 2, 4, 8, 9, 3, 7, 6},
+            {7, 3, 9, 2, 5, 6, 8, 4, 1},
+            {4, 6, 8, 3, 7, 1, 2, 9, 5},
+            {3, 8, 7, 1, 2, 4, 6, 5, 9},
+            {5, 9, 1, 7, 6, 3, 4, 2, 8},
+            {2, 4, 6, 8, 9, 5, 7, 1, 3},
+            {9, 1, 4, 6, 3, 7, 5, 8, 2},
+            {6, 2, 5, 9, 4, 8, 1, 3, 7},
+            {8, 7, 3, 5, 1, 2, 9, 6, 4}
     };
     private int[][] gameBoardThreeSolution = {
             {2, 3, 5, 1, 4, 7, 9, 8, 6},
@@ -67,54 +86,26 @@ public class Board {
      * constructor for Board 
      * randomly picks a board as new board 
      */
-    public Board(int rand) {
-        //remember what rand was so we can find the solution. 
-        this.boardPicked = rand;
+    public Board() {
+        Random random = new Random();
+        int rand = random.nextInt(3);
         switch (rand) {
-        case 1:
+        case 0:
             this.board = gameBoardOne;
+            this.solution = gameBoardOneSolution;
+            break;
+        case 1:
+            this.board = gameBoardTwo;
+            this.solution = gameBoardTwoSolution;
             break;
         case 2:
-            this.board = gameBoardTwo;
-            break;
-        case 3:
             this.board = gameBoardThree;
+            this.solution = gameBoardThreeSolution;
             break;
-        case 6:
-            this.board = gameBoardThreeSolution;
-        //removed the default because it would interfere with getting the solution. 
-        // default:
-        //     this.board = gameBoardOne;
-        //     break;
         }
     }
-    protected void printSolution(){
-        System.out.println("   1  2  3   4  5  6   7  8  9  ");
-        for(int i=0; i< board.length; i++){
-            if(i%3 == 0) { // print 3*3 row border
-                System.out.println(" |---------+---------+---------|");
-            }
-            // start row -> "%s| %d  %d' %d'| %d  %d  %d | %d  %d' %d | \n"
-            String row = ""; 
-            row = row+ (char) ('A'+i); // add letter to row
-            for(int j = 0; j<board[0].length; j++){
-                // adds 3*3 column border
-                if(j%3 == 0) {
-                    row = row + "|";
-                }
-                int cellNum = board[i][j];
-                row = row + " " + cellNum +" ";
-                // add row border
-                if(j == 8) {
-                    row = row + "|";
-                }
-            }
-            System.out.println(row); // print row
-            if(i == 8) {            // print last border
-                System.out.println(" |---------+---------+---------|");
-            }
-        }
-    }
+
+
     /**
      * prints initial game board at start of game
      * and adds unchangeable cells to set
@@ -156,7 +147,7 @@ public class Board {
                 if(j == 8) {
                     row = row + "|";
                 }
-            } // end forloop
+            } // end for loop
             System.out.println(row); // print row
             if(i == 8) {            // print last border
                 System.out.println(" |---------+---------+---------|");
@@ -173,17 +164,50 @@ public class Board {
      */
     protected void printInstructions() {
         System.out.println("Welcome to Sudoku.\n\n"
-                + "- To enter a number in a cell, input the cell ID "
-                + "\ni.e. the row letter and column number together "
-                + "followed by the number you would like to be in the cell"
-                + " E.g. A1 8 \n"
-                + "- To delete a number you entered from a cell, enter \"undo\" followed by "
-                + "the cell ID. E.g. undo A1 \n"
-                + "- To swap two cells, input \"swap\" and both cell IDs. E.g. Swap A1 F6 \n"
-                + "- To get instructions again enter \"help\" \n"
-                + "- To restart the game, input \"restart\" \n"
-                + "- To end the game after completing the board, input \"end\" \n");
+                + "Remember to leave a space between commands. \n"
+                + "- To enter a number use cellID + number "
+                + "E.g. A1 8\n"
+                + "- To enter into multiple cells use \"enter\" + cell ID + number \n"
+                + " E.g. enter A1 1 B4 6 \n"
+                + "- To delete a cell entry use \"undo\" + cell ID "
+                + "E.g. undo A1 \n"
+                + "- To swap two cells, use \"swap\" + cellID + cellID. \n"
+                + "E.g. Swap A1 F6 \n"
+                + "- To get instructions again use the \"help\" command\n"
+                + "- To restart the game, use the \"restart\" command\n"
+                + "- To end the game after completing the board, use \"end\" command \n");
         System.out.println("Have fun playing Sudoku! Here is your initial board");
+    }
+
+    /***
+     * prints solution board to console
+     */
+    protected void printSolution(){
+        System.out.println("   1  2  3   4  5  6   7  8  9  ");
+        for(int i=0; i< solution.length; i++){
+            if(i%3 == 0) { // print 3*3 row border
+                System.out.println(" |---------+---------+---------|");
+            }
+            // start row -> "%s| %d  %d' %d'| %d  %d  %d | %d  %d' %d | \n"
+            String row = "";
+            row = row+ (char) ('A'+i); // add letter to row
+            for(int j = 0; j<solution[0].length; j++){
+                // adds 3*3 column border
+                if(j%3 == 0) {
+                    row = row + "|";
+                }
+                int cellNum = solution[i][j];
+                row = row + " " + cellNum +" ";
+                // add row border
+                if(j == 8) {
+                    row = row + "|";
+                }
+            }
+            System.out.println(row); // print row
+            if(i == 8) {            // print last border
+                System.out.println(" |---------+---------+---------|");
+            }
+        }
     }
 
     /**
@@ -250,17 +274,21 @@ public class Board {
             return false;
         }
     }
-    public boolean isEqual(int[][] board1, int[][] board2){
-        for (int i = 0; i < board1.length; i++){
-            for (int j = 0; j<board[0].length; j++){
-                if (board1[i][j] != board2[i][j]){
+
+
+    /**
+     * compares board to solution for correctness
+     * @return true if puzzle is complete
+     */
+    public boolean isCorrectSolution() {
+        for (int i = 0; i < board.length; i++){
+            for (int j = 0; j<solution.length; j++){
+                if (board[i][j] != solution[i][j]){
                     return false;
                 }
             }
         }
         return true;
-
     }
-
 
 }
